a1c0ca1edd0e8589b3283c38de46de52f56a0ebc
Fab <redstoneinfire@gmail.com>
2017-03-28 11:26:08 +0200

    ARE YOU HAPPY NOW

--- src/MiniProjet.java
+++ src/MiniProjet.java
@@ -46,43 +46,56 @@ public class MiniProjet implements CommandListener {
   public void commandEntered(ArrayList<String> s) {
     Interface.setStatus("Processing command");
     switch(s.get(0)) {
-      case "help": for(String str : allCommandes) log(str + ": " + doc.get(str) + "\n"); break;
+      case "help":
+          for(String str : allCommandes)
+            log(str + ": " + doc.get(str) + "\n");
+        break;
       case "clear": Interface.clear(); break;
       case "exit": System.exit(0); break;
-  	  case "print": for (String args : s.subList(1, s.size())) log(args); break;
+  	  case "print":
+          for (String args : s.subList(1, s.size()))
+            log(args);
+        break;
       case "afficherFlotte": log(flotte.toString()); break;
   	  case "afficherPersonnel": log(personnel.toString()); break;
-  	  case "ajouterVaisseau": if (s.size() == 3) {flotte.ajouterVaisseau(new Vaisseau(s.get(1), s.get(2))); log("Vaisseau ajoute !");} else log(doc.get("ajouterVaisseau")); break;
+  	  case "ajouterVaisseau":
+          if (s.size() == 3) {
+            flotte.ajouterVaisseau(new Vaisseau(s.get(1), s.get(2)));
+            log("Vaisseau ajoute !");
+          } else
+            log(doc.get("ajouterVaisseau"));
+        break;
       case "ajouterPersonnel":
-    		if (s.size() == 4) {
-    			personnel.ajouterMembre(new Membre(s.get(1), s.get(2).charAt(0), s.get(3))); log("Membre ajoute");
-    		} else if (s.size() == 5) {
-    			switch (s.get(1)) {
-    				case "libere": personnel.ajouterMembre(new Libere(s.get(2), s.get(3).charAt(0), s.get(4))); log("Membre libere ajoute !"); break;
-    				case "operateur": personnel.ajouterMembre(new Operateur(s.get(2), s.get(3).charAt(0), s.get(4))); log("Operateur ajoute !"); break;
-    				default: log(doc.get("ajouterPersonnel"));
-    			}
-    		} else
-    			log(doc.get("ajouterPersonnel"));
-  		break;
+      		if (s.size() == 4) {
+      			personnel.ajouterMembre(new Membre(s.get(1), s.get(2).charAt(0), s.get(3))); log("Membre ajoute");
+      		} else if (s.size() == 5) {
+      			switch (s.get(1)) {
+      				case "libere": personnel.ajouterMembre(new Libere(s.get(2), s.get(3).charAt(0), s.get(4))); log("Membre libere ajoute !"); break;
+      				case "operateur": personnel.ajouterMembre(new Operateur(s.get(2), s.get(3).charAt(0), s.get(4))); log("Operateur ajoute !"); break;
+      				default: log(doc.get("ajouterPersonnel"));
+      			}
+      		} else
+      			log(doc.get("ajouterPersonnel"));
+    		break;
   	  case "modifierEquipage":
-      	if(s.size() == 3) {
-        	if(flotte.getByName(s.get(1)) != null){
-      			if(personnel.getByName(s.get(2)) != null){
-      				if(!flotte.getByName(s.get(1)).supprimerMembre(s.get(2)))
-      					flotte.getByName(s.get(1)).ajouterMembre(personnel.getByName(s.get(2)));
-      			} else log(s.get(2) + ": cette personne ne fait pas partie de votre personnel."); break;
-      		} else log(s.get(1) + ": ce vaisseau ne fait pas partie de votre flotte.");
-        } else log(doc.get("modifierEquipage"));
-      break;
+        	if(s.size() == 3) {
+          	if(flotte.getByName(s.get(1)) != null){
+        			if(personnel.getByName(s.get(2)) != null){
+        				if(!flotte.getByName(s.get(1)).supprimerMembre(s.get(2)))
+        					flotte.getByName(s.get(1)).ajouterMembre(personnel.getByName(s.get(2)));
+        			} else log(s.get(2) + ": cette personne ne fait pas partie de votre personnel."); break;
+        		} else log(s.get(1) + ": ce vaisseau ne fait pas partie de votre flotte.");
+          } else log(doc.get("modifierEquipage"));
+        break;
       case "DragAndDrop": drag = new DragNDrop(flotte.getByName(s.get(1)).getEquipage()); break;
       case "addList": drag.addList(flotte.getByName(s.get(1)).getEquipage()); break;
       case "infiltrerAgent":
-        if(s.size() > 1) {
-          if(personnel.getByName(s.get(1)) != null && personnel.getByName(s.get(1)) instanceof Libere) {
-            matrix.infiltrerAgent((Libere)personnel.getByName(s.get(1)));
+          if(s.size() > 1) {
+            if(personnel.getByName(s.get(1)) != null && personnel.getByName(s.get(1)) instanceof Libere) {
+              matrix.infiltrerAgent((Libere)personnel.getByName(s.get(1)));
+            }
           }
-        } break;
+        break;
   	  default: log(s.get(0) + " : commande non reconnue. Tapez 'help' pour avoir une liste des commandes disponibles");
     }
     Interface.setStatus("Idle");
