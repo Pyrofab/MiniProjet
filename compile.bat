51d9ffb210b26b695f9198cdd97f543e32c52c1d
Fabien <redstoneinfire@gmail.com>
2017-03-29 14:48:53 +0200

    Affichage de la matrice beta

--- compile.bat
+++ compile.bat
@@ -1,6 +1,6 @@
 javac -cp src -d bin -Xlint:unchecked src/*.java
 cd bin
-jar cvmf0 ..\META-INF\MANIFEST.MF MiniProjet.jar es\esy\ladysnake\gui\*.class es\esy\ladysnake\miniprojet\*.class
+jar cvmf0 ..\META-INF\MANIFEST.MF MiniProjet.jar es\esy\ladysnake\gui\*.class es\esy\ladysnake\miniprojet\main\*.class es\esy\ladysnake\miniprojet\display\*.class
 cd ..
 move bin\MiniProjet.jar .
 java -jar MiniProjet.jar
