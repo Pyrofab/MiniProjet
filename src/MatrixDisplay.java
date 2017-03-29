d169961eb60a387024d2c9ecb9e02cf2ec23cb5c
Fabien <redstoneinfire@gmail.com>
2017-03-29 15:48:13 +0200

    Beta affichage matrice terminee

--- src/MatrixDisplay.java
+++ src/MatrixDisplay.java
@@ -41,18 +41,21 @@ public class MatrixDisplay extends JPanel implements Runnable, ActionListener {
   public void paintComponent(Graphics g) {
     g.clearRect(0,0,this.getWidth(), this.getHeight());
     g.setColor(Color.black);
-    g.fillRect(0, exit.getHeight(), this.getWidth(), this.getHeight());
+    g.fillRect(0, exit.getHeight() + 10, this.getWidth(), this.getHeight());
     g.setColor(Color.green);
     for(int i = 0; i < this.getWidth(); i+=this.getWidth()/10) {
-      g.drawLine(i, exit.getHeight(), i, this.getHeight());
+      g.drawLine(i, exit.getHeight() + 10, i, this.getHeight());
     }
     int index = 0;
-    for (int j = exit.getHeight(); j < this.getHeight(); j += (this.getHeight() - exit.getHeight())/10) {
+    for (int j = exit.getHeight() + 10; j < this.getHeight(); j += (this.getHeight() - exit.getHeight() - 10)/10) {
       g.drawLine(0, j, this.getWidth(), j);
     }
     for (int x = 1; x < 10; x++){
+      g.drawString(x + "", 20 + x * (this.getWidth())/10, (this.getHeight() - exit.getHeight() - 10)/10);
       for (int y = 1; y < 10; y++) {
-        g.drawString(y + "", 10, exit.getHeight() + (y+1)*(this.getHeight() - exit.getHeight())/10 - 20);
+        g.drawString(y + "", 10, exit.getHeight() + 10 + (y+1)*(this.getHeight() - exit.getHeight() - 10)/10 - 20);
+        // g.drawString((10*(x-1) + y) +"", 20 + x * (this.getWidth())/10, exit.getHeight() + 10 + (y+1)*(this.getHeight() - exit.getHeight() - 10)/10 - 20);
+        g.drawString((matrix.get(x, y)) +"", 20 + x * (this.getWidth())/10, exit.getHeight() + 10 + (y+1)*(this.getHeight() - exit.getHeight() - 10)/10 - 20);
       }
     }
   }
